860914fc509b1fad8b4e1fd5eca2a23b198b2a5d
jiangjuhong <690717394@qq.com>
2021-09-30 14:43:44 +0800

    feat 更新数据

--- main.py
+++ main.py
@@ -9,38 +9,67 @@ import json
 import pinyin
 
 
-# 请求政府网，获得数据结构
-def getText():
-    res = requests.get("http://www.gov.cn/test/2011-08/22/content_1930111.htm")
+# 请求政府网，获得数据结构，返回行政区划代码、名称、级别
+# 统一规范，仅返回 code、name、level数据。直辖市需要在 level 0 和 level 1 中均出现
+def parse():
+    # 读取网页
+    res = requests.get("http://www.mca.gov.cn/article/sj/xzqh/2020/20201201.html")
     if res.status_code != 200:
         raise Exception("网页请求失败!")
 
-    # 网页源码
-    data = str(res.content, 'utf-8')
+    # 解析数据
+    return _parse_20201201(bs4.BeautifulSoup(str(res.content, 'utf-8')))
 
-    # 解析节点，获得文本形式的数据
-    soup = bs4.BeautifulSoup(data)
-    return soup.span.text
+
+# 解析 民政部 20201201 数据
+# 统一规范，仅返回 code、name、level数据。直辖市需要在 level 0 和 level 1 中均出现
+def _parse_20201201(soup):
+    valid_data = []
+    # 获得表格，并过滤前三行的标题行以及后面的港澳台信息
+    trs = soup.table.findAll('tr')[3:-12]
+    for tr in trs:
+        tds = tr.findAll('td')
+        tds = list(filter(lambda item: item.text != '', tds))
+        if len(tds) < 2:
+            continue
+
+        # 获得城市编码，名称，级别
+        code = tds[0].text
+        name = tds[1].text
+        level = name.count(' ')
+        if level > 2:
+            level = 2
+
+        # 追加数据
+        valid_data.append({
+            "code": code,
+            "name": name.strip(),
+            "level": level,
+        })
+
+        # 如果是直辖市，则添加市数据
+        if ["110000", "120000", "310000", "500000"].count(code):
+            valid_data.append({
+                "code": code,
+                "name": name.strip(),
+                "level": 1,
+            })
+
+    return valid_data
 
 
 if __name__ == '__main__':
-    text = getText()
     result = []
+    data = parse()
 
     parentCode = {}
     lastLevel = None
     lastCode = None
-    for item in text.split("\n"):
-        if item == '':
-            continue
-
-        # 分隔字符串行政区划代码和区县
-        its = item.split("  ")
-
+    for item in data:
         # 层级、代码、名称
-        level = item.count("\xa0\xa0")
-        code = its[0].strip()
-        name = its[1]
+        level = item["level"]
+        code = item["code"]
+        name = item["name"]
         py = ""
 
         # 层级发生改变时更新层级关系
